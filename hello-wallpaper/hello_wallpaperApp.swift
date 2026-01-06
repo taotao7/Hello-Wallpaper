a0abd5b5484a89f7d9c105094e5d77b8b58fc940
taotao7 <moca_tao7@foxmail.com>
2026-01-06 14:27:35 +0800

    Initial Commit

---
+++ hello-wallpaper/hello_wallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_wallpaperApp.swift
+//  hello-wallpaper
+//
+//  Created by tao on 2026/1/6.
+//
+
+import SwiftUI
+
+@main
+struct hello_wallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
